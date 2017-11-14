7025c4b1efd814f4aa197b1d36768604e15d9c9c
Märt Reose <mart.reose@smit.ee>
2017-11-14 10:50:04 +0200

    enable redis

--- 12f_app/src/main/java/com/github/m2rt/app/StockSpringBootStarter.java
+++ 12f_app/src/main/java/com/github/m2rt/app/StockSpringBootStarter.java
@@ -2,8 +2,11 @@ package com.github.m2rt.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+
+@EnableRedisHttpSession
 public class StockSpringBootStarter {
     public static void main(String[] args) {
             SpringApplication.run(StockSpringBootStarter.class, args);
