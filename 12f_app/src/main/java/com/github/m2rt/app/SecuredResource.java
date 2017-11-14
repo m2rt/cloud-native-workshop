acab0bd73712c115944bf5a553baa75f58dca260
Märt Reose <mart.reose@smit.ee>
2017-11-14 10:24:14 +0200

    hotfix

--- 12f_app/src/main/java/com/github/m2rt/app/SecuredResource.java
+++ 12f_app/src/main/java/com/github/m2rt/app/SecuredResource.java
@@ -1,5 +1,7 @@
 package com.github.m2rt.app;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
